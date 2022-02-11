32a711207a0b4b89a74ca5a950d54ab1822c4caf
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-02-11 13:30:14 -0300

    ajustes de erros dos requisitos 1 ao 4

--- services/serviceLogin.js
+++ services/serviceLogin.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+
 const { checkLoginSchema } = require('./schemasValidation');
 const errorConstructor = require('../utils/functions/index');
 const status = require('../utils/codes');
 const { Users } = require('../models');
 const { invalidFields } = require('../utils/messages');
 
-const SECRET = 'secret';
-const EXPIRES = { expiresIn: '7d', algorithm: 'HS256' };
+const SECRET = process.env.SECRET || 'secret';
+const EXPIRES = { expiresIn: '2h', algorithm: 'HS256' };
 
 const serviceLogin = async (requestBody) => {
   const { error } = checkLoginSchema.validate(requestBody);
